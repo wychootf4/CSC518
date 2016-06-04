23c3c609bced034451532409f74fe208e89ccef3
wychootf4 <hootf4@gmail.com>
2016-06-04 22:59:31 +0100

    Add slides

---
+++ week1/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Yuchen Wang on 6/4/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("First code in CSC518.");
+    }
+}
